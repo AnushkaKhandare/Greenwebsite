0bcc3b0f69cd32daa3e6add229820007e2a84929
AnushkaKhandare <152138912+AnushkaKhandare@users.noreply.github.com>
2023-12-11 08:29:11 +0530

    Add files via upload

---
+++ khandare.js
@@ -0,0 +1,50 @@
+let list = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list . items');
+let dots = document.querySelectorAll('.slider .dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+
+next.onclick = function (){
+    if (active + 1 > lengthItems) {
+        active = 0;
+    }else {
+        active = active + 1;
+    }
+    reloadSlider();
+
+}
+
+prev.onclick = function (){
+
+    if(active -1 < 0 ){
+        active = lengthItems;
+    }else{
+        active = active -1;
+    }
+    reloadSlider();
+}
+
+function reloadSlider() {
+
+    let checkleft = items[active].offsetLeft;
+
+    list.style.left = -checkleft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider.dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+}
+
+dots.forEach((li, key) => {
+
+    li.addEventListener('click', function () {
+
+        active = key;
+        reloadSlider();
+    })
+})
